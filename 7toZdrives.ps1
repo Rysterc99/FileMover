04c01c8e759bca42e4193745465bc2a029102afc
Rysterc99 <46036119+Rysterc99@users.noreply.github.com>
2018-12-20 18:22:46 -0500

    Add files via upload

---
+++ 7toZdrives.ps1
@@ -0,0 +1,41 @@
+﻿$env:UserName
+
+Test-Path -Path Z:\
+
+##/    Creates 2 Folders in the Z Drive          \##
+New-Item -ItemType directory -Path Z:\Windows7Data
+New-Item -ItemType directory -Path Z:\FAVORITES
+####################################################
+
+#####/     Manually Moves the contents of the D Drive to the Z Drive                     ###\
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Favorites" -Destination Z:\FAVORITES -Recurse
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Contacts" -Destination Z:\Windows7Data -Recurse
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Desktop" -Destination Z:\Windows7Data -Recurse
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Downloads" -Destination Z:\Windows7Data -Recurse
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Favorites" -Destination Z:\Windows7Data -Recurse
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Links" -Destination Z:\Windows7Data -Recurse
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Documents" -Destination Z:\Windows7Data -Recurse 
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Music" -Destination Z:\Windows7Data -Recurse     #Gets a error when copied saying access to the path is Denied but it still copies the contents
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Pictures" -Destination Z:\Windows7Data -Recurse  #Gets a error when copied saying access to the path is Denied but it still copies the contents
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Videos" -Destination Z:\Windows7Data -Recurse    #Gets a error when copied saying access to the path is Denied but it still copies the contents
+
+Copy-Item -Path "D:\Users\$($env:UserName)\Searches" -Destination Z:\Windows7Data -Recurse
+##############################################################################################\
+
+
+Get-ChildItem -Path Z:\Windows7Data -Force # Shows that the Files were actually copied in terminal 
+
+Get-ChildItem -Path Z:\FAVORITES -Force 
+
+
+Test-Path -Path Z:\ # Confirms that the 2 folders were successfully created
---
+++ Z-driveto10.ps1
@@ -0,0 +1,12 @@
+﻿#Z-Drive to Windows 10
+#
+# Script Currently Work's to pull the Folders off of the Z-Drive and merge them with the Existing Folders on Windows 10
+#
+$env:UserName
+
+$path = "\\dutchess\data\HomeStore01\$($env:UserName)\Windows7Data" # gets the path from the server to the Z-Drive
+
+$outfile =  "C:\Users\$($env:UserName)" #Gets the File Path the Person's C-Drive 
+
+Get-ChildItem -Path $path | Get-Content | Add-Content -Path $outfile﻿ -Force #Takes the folders from inside Windows7Data and merges them with the existing files in the C Drive
+
